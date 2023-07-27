a4b95d5cc2f5ed4cab3e77613bea0d2ce53ecada
whisnumurtyga <whisnumurty.galih.21@student.if.ittelkom-sby.ac.id>
2023-07-27 11:41:02 +0700

    done testing relation transaction with other

--- app/Http/Livewire/HistoryTransactionShow.php
+++ app/Http/Livewire/HistoryTransactionShow.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Transaction;
 use Livewire\Component;
 
 class HistoryTransactionShow extends Component
 {
     public function render()
     {
-        return view('livewire.history-transaction-show');
+        return view('livewire.history-transaction-show',[
+            'transactions' => $this->getTransactions()
+        ]);
+    }
+
+    public function getTransactions()
+    {
+        $query = Transaction::with(['users', 'members', 'status', 'payments'])->orderByDesc('id');
+        $transacttions = $query->get();
+
+        return $transacttions;
     }
 }
--- resources/views/livewire/history-transaction-show.blade.php
+++ resources/views/livewire/history-transaction-show.blade.php
@@ -1,4 +1,7 @@
 <div>
     {{-- Care about people's approval and you will be their prisoner. --}}
     <h1>tetetes</h1>
+    {{ $transactions[0]->members->name }}
+    {{ $transactions[0]->users->name }}
+    {{ $transactions[0]->payments->name }}
 </div>
